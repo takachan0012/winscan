3d4587f844faef593fad93ad50dd6ce6bf492c8e
winsnip-official <winsnip.official@gmail.com>
2025-12-22 07:51:26 +0700

    Add service worker to fix PWA registration error

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+// Simple Service Worker for WinScan
+const CACHE_NAME = 'winscan-v1';
+const urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let all requests pass through - no caching for now
+  event.respondWith(fetch(event.request));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
